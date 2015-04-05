aed1bc576c241953b54e7de3f13dd9d24811c49e
soidid <soidid@gmail.com>
2015-04-05 22:27:23 +0800

    fix 罷免 many versions in many stage. add other party's version

--- src/data/dismiss.js
+++ src/data/dismiss.js
@@ -30,25 +30,26 @@ module.exports = {
           "currentLaw" : "依據《公職人員選舉罷免法》76條，提案罷免人數須在原選舉區選舉人總數2%以上；選罷法77條，現役軍人、服替代役之現役役男或公務人員，不得為罷免案提議人",
           "progress" : "程序委員會",
           "proposedBill" : [
-              { 
-                "proposer" : "高志鵬",
-                "summary" : "罷免提案門檻降至百分之一",
-                "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-76",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
-              },
-              { 
-                "proposer" : "李應元",
-                "summary" : "將提議人數之計算基數，由「原選舉區選舉人」改正為「應屆當選投票人總數」，並刪除「現役軍人、服替代役之現役役男或公務人員，不得為罷免案提議人」規定。",
-                "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-76",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
+              {
+                "progress" : "程序委員會",
+                "bills":[
+                  { 
+                   "proposer" : "高志鵬",
+                   "summary" : "罷免提案門檻降至百分之一",
+                   "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-76"
+                  },
+                  { 
+                   "proposer" : "李應元",
+                   "summary" : "將提議人數之計算基數，由「原選舉區選舉人」改正為「應屆當選投票人總數」，並刪除「現役軍人、服替代役之現役役男或公務人員，不得為罷免案提議人」規定。",
+                   "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-76"
+                  },
+                  {
+                    "proposer" : "台聯黨黨團",
+                    "summary" : "將提議人數之計算基數，由「原選舉區選舉人」改正為「原次選舉原選舉區投票總數」",
+                    "link" : "http://ly.g0v.tw/bills/1044L15652#proposed-76"
+                  }
+                ]
               }
-
           ],
           "govState" : "罷免影響政治秩序安定，制度上有一定限制。但門檻攸關人民參政權，支持理性討論"
         },
@@ -60,32 +61,29 @@ module.exports = {
           "currentLaw" : "依據《公職人員選舉罷免法》80條，立委、直轄市議員、市長，縣市首長的罷免案，連署期為30天；選罷法81條，連署罷免人數須在原選舉區選舉人總數13%以上",
           "progress" : "程序委員會",
           "proposedBill" : [
-              { 
-                "proposer" : "高志鵬",
-                "summary" : "罷免連署門檻降至百分之六",
-                "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-81",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
-              },
-              { 
-                "proposer" : "李應元",
-                "summary" : "各類公職罷免連署期間延長十～十五日",
-                "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-80",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
-              },
-              { 
-                "proposer" : "陳其邁",
-                "summary" : "各類公職罷免連署期間延長十五日",
-                "link" : "http://ly.g0v.tw/bills/1044L16897#proposed-80",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
+              {
+                "progress" : "程序委員會",
+                "bills":[
+                  { 
+                    "proposer" : "高志鵬",
+                    "summary" : "罷免連署門檻降至百分之六",
+                    "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-81"
+                  },
+                  { 
+                    "proposer" : "李應元",
+                    "summary" : "各類公職罷免連署期間延長十～十五日",
+                    "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-80"
+                  },
+                  { 
+                    "proposer" : "陳其邁",
+                    "summary" : "各類公職罷免連署期間延長十五日",
+                    "link" : "http://ly.g0v.tw/bills/1044L16897#proposed-80"
+                  },
+                  {
+                    "proposer" : "台聯黨黨團",
+                    "summary" : "各類公職罷免連署期間延長五～十日",
+                    "link" : "http://ly.g0v.tw/bills/1044L15652#proposed-80"
+                  }]
               }
           ],
           "govState" : "罷免影響政治秩序安定，制度上有一定限制。但門檻攸關人民參政權，支持理性討論"
@@ -98,24 +96,26 @@ module.exports = {
           "currentLaw" : "依據《公職人員選舉罷免法》90條，「罷免案投票人數不足原選舉區選舉人總數二分之一以上或同意罷免票數未超過有效票二分之一以上」，均視為罷免案遭到否決。",
           "progress" : "程序委員會",
           "proposedBill" : [
-              { 
-                "proposer" : "高志鵬",
-                "summary" : "有效同意票數多於不同意票數，且同意票數達原選舉區選舉人總數四分之一以上，即為通過",
-                "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-90",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
-              },
-              { 
-                "proposer" : "李應元",
-                "summary" : "同意罷免票數未超過有效票數二分之一以上者為否決。（刪除投票人數須達原選舉區選舉人總數二分之一以上）",
-                "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-90",
-                "withdraw" : {
-                    "description" : "退回程序",
-                    "link" : ""
-                 }
-              }
+              {
+                "progress" : "程序委員會",
+                "bills":[
+                { 
+                  "proposer" : "高志鵬",
+                  "summary" : "有效同意票數多於不同意票數，且同意票數達原選舉區選舉人總數四分之一以上，即為通過",
+                  "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-90"
+                },
+                { 
+                  "proposer" : "李應元",
+                  "summary" : "同意罷免票數未超過有效票數二分之一以上者為否決。（刪除「投票人數須達原選舉區選舉人總數二分之一以上」之規定）",
+                  "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-90"
+                },
+                {
+                    "proposer" : "台聯黨黨團",
+                    "summary" : "同意罷免票數超過有效票二分之一以上為通過。（刪除「投票人數須達原選舉區選舉人總數二分之一以上」之規定）",
+                    "link" : "http://ly.g0v.tw/bills/1044L15652#proposed-90"
+                }
+                ]
+              }  
           ],
           "govState" : "罷免影響政治秩序安定，制度上有一定限制。但門檻攸關人民參政權，支持理性討論"
         },
@@ -127,16 +127,21 @@ module.exports = {
           "currentLaw" : "依據《公職人員選舉罷免法》81條，同一罷免案的提議人不得為連署人，應分開計算；選罷法87條，罷免投票不得與其他選舉同時舉行",
           "progress" : "程序委員會",
           "proposedBill" : [
-              { 
-                "proposer" : "陳其邁",
-                "summary" : "該期間內有其他各類選舉時，應同時舉行投票",
-                "link" : "http://ly.g0v.tw/bills/1044L16897#proposed-87"   
-              },
-              { 
-                "proposer" : "李應元",
-                "summary" : "增訂公職人員選舉罷免法第八十條之一，明定罷免連署人名冊格式，每張以一位連署人為限",
-                "link" : "http://lis.ly.gov.tw/lgcgi/lgmeetimage?cfc7cfc9cec7c8cdc5c8ccd2c8cb"   
-              }
+            {
+                "progress" : "程序委員會",
+                "bills":[
+                  { 
+                    "proposer" : "陳其邁",
+                    "summary" : "該期間內有其他各類選舉時，應同時舉行投票",
+                    "link" : "http://ly.g0v.tw/bills/1044L16897#proposed-87"   
+                  },
+                  { 
+                    "proposer" : "李應元",
+                    "summary" : "增訂公職人員選舉罷免法第八十條之一，明定罷免連署人名冊格式，每張以一位連署人為限",
+                    "link" : "http://lis.ly.gov.tw/lgcgi/lgmeetimage?cfc7cfc9cec7c8cdc5c8ccd2c8cb"   
+                  }
+                ]
+            }
           ],
           "govState" : "支持刪除「罷免不得宣傳」"
         }
@@ -163,16 +168,51 @@ module.exports = {
           "currentLaw" : "依據《公職人員選舉罷免法》86條，罷免可設辦事處，但除了連署支必要活動之外，不得有宣傳罷免的行為",
           "progress" : "黨團協商",
           "proposedBill" : [
-              { 
-                "proposer" : "李俊俋",
-                "summary" : "廢除罷免不得宣傳",
-                "link" : "http://ly.g0v.tw/bills/1044L16151#proposed-86"   
-              },
-              { 
-                "proposer" : "邱志偉",
-                "summary" : "廢除罷免不得宣傳",
-                "link" : "http://ly.g0v.tw/bills/1044L16467#proposed-86"   
-              }
+              {
+                "progress" : "黨團協商",
+                "bills":[
+                    { 
+                      "proposer" : "李俊俋",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16151#proposed-86"   
+                    },
+                    { 
+                      "proposer" : "邱志偉",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16467#proposed-86"   
+                    },
+                    { 
+                      "proposer" : "台聯黨黨團",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16447"   
+                    }
+                ]
+             },
+             {
+                "progress" : "程序委員會",
+                "bills":[
+                    { 
+                      "proposer" : "高志鵬",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16882#proposed-86"   
+                    },
+                    { 
+                      "proposer" : "李應元",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16496#proposed-86"   
+                    },
+                    { 
+                      "proposer" : "李昆澤",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L15462#proposed-86"   
+                    },
+                    { 
+                      "proposer" : "薛凌",
+                      "summary" : "廢除罷免不得宣傳",
+                      "link" : "http://ly.g0v.tw/bills/1044L16883#proposed-86"   
+                    }
+                ]
+             }
           ],
           "govState" : "支持刪除「罷免不得宣傳」"
         }
